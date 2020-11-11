39b0a917b74e22a24a933cd30e53375351872a16
Jeremy Greer <jex.grizzle@gmail.com>
2020-11-10 21:42:11 -0500

    Handle simple object

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,36 +1,6 @@
-import * as mod from ".";
+import { getTypeDefintionForValue, TypeDefintion, getTypesForObject } from ".";
 
 describe("getTypeDefintionForValue", () => {
-  type TypeDefintion = {
-    type: "string" | "number" | "boolean" | "null";
-  };
-  type Value = string | number | boolean | null;
-  const getTypeFromObject = (value: Value): TypeDefintion["type"] => {
-    if (value === null) {
-      return "null";
-    }
-  };
-
-  const getType = (value: Value): TypeDefintion["type"] => {
-    const type = typeof value;
-    switch (type) {
-      case "string":
-      case "number":
-      case "boolean":
-        return type;
-      case "object":
-        return getTypeFromObject(value);
-      default:
-        throw new Error(`unhandled case "${type}"`);
-    }
-  };
-
-  const getTypeDefintionForValue = (value: Value): TypeDefintion => {
-    return {
-      type: getType(value),
-    };
-  };
-
   it("should return the type for `string`", () => {
     const value = "Prodigy";
     const actual = getTypeDefintionForValue(value);
@@ -72,6 +42,51 @@ describe("getTypeDefintionForValue", () => {
     const actual = getTypeDefintionForValue(value);
     const expected: TypeDefintion = {
       type: "null",
+    };
+    expect(actual).toEqual(expected);
+  });
+
+  describe("complex types", () => {
+    it("should throw for arrays", () => {
+      expect(() => {
+        getTypeDefintionForValue([] as any);
+      }).toThrow(/not a simple object/);
+    });
+
+    it("should throw for {}", () => {
+      expect(() => {
+        getTypeDefintionForValue({} as any);
+      }).toThrow(/not a simple object/);
+    });
+  });
+});
+
+describe("getTypesForObject", () => {
+  it("should handle flat objects", () => {
+    const value = {
+      a: "apple",
+      b: false,
+      c: true,
+      d: 999,
+      e: null,
+    };
+    const actual = getTypesForObject(value);
+    const expected = {
+      a: {
+        type: "string",
+      },
+      b: {
+        type: "boolean",
+      },
+      c: {
+        type: "boolean",
+      },
+      d: {
+        type: "number",
+      },
+      e: {
+        type: "null",
+      },
     };
     expect(actual).toEqual(expected);
   });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,49 @@
-export const print = (message: string): void => {
-  console.log(message)
-}
+export type TypeDefintion = {
+  type: "string" | "number" | "boolean" | "null";
+};
+
+type Value = string | number | boolean | null;
+
+const getTypeFromObject = (value: Value): TypeDefintion["type"] => {
+  if (value === null) {
+    return "null";
+  }
+  throw new Error("this is not a simple object");
+};
+
+const getType = (value: Value): TypeDefintion["type"] => {
+  const type = typeof value;
+  switch (type) {
+    case "string":
+    case "number":
+    case "boolean":
+      return type;
+    case "object":
+      return getTypeFromObject(value);
+    default:
+      throw new Error(`unhandled case "${type}"`);
+  }
+};
+
+export const getTypeDefintionForValue = (value: Value): TypeDefintion => {
+  return {
+    type: getType(value),
+  };
+};
+
+type ObjectValue = {
+  [key: string]: Value;
+};
+
+export type ObjectTypes = {
+  [key: string]: TypeDefintion;
+};
+
+export const getTypesForObject = (objectValue: ObjectValue): ObjectTypes => {
+  return Object.keys(objectValue).reduce((acc, key) => {
+    return {
+      ...acc,
+      [key]: getTypeDefintionForValue(objectValue[key]),
+    };
+  }, {});
+};
